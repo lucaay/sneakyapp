f30e1b508a02b89a88bcaba40dc1df40381a5581
Luca Sergiu <you@example.com>
2022-05-18 19:03:13 +0300

    changes to login

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,30 +1,42 @@
-import React from "react";
+import React, { useRef } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import styles from "./LogIn.module.css";
 
 const LogIn = () => {
+    const usernameRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        const usernameValue = usernameRef.current.value;
+        const passwordValue = passwordRef.current.value;
+        console.log(usernameValue, passwordValue);
+    };
+
     return (
-        <form>
+        <form onSubmit={onSubmitHandler}>
             <div>
                 <TextField
                     id="outlined-name"
                     label="Username"
                     type="username"
-                    // value={}
-                    // onChange={}
+                    ref={usernameRef}
                     required
                 />
                 <TextField
                     id="outlined-name"
                     label="Password"
                     type="password"
-                    // value={}
-                    // onChange={}
+                    ref={passwordRef}
                     required
                 />
             </div>
-            <Button variant="contained">LOGIN</Button>
+            <Button variant="contained" type="submit">
+                LOGIN
+            </Button>
             <div>
                 <p>Dorești să te înregistrezi?</p>
                 <Link to="/signup">Creează un cont.</Link>
