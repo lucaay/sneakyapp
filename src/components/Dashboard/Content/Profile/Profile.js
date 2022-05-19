9699a0a3bc08b10e6c8c88cb63e8392897cfe02a
Luca Sergiu <you@example.com>
2022-05-19 23:33:45 +0300

    profile

--- src/components/Dashboard/Content/Profile/Profile.js
+++ src/components/Dashboard/Content/Profile/Profile.js
@@ -2,7 +2,18 @@ import React from "react";
 import styles from "./Profile.module.css";
 
 const Profile = () => {
-    return <div className={styles["profile-container"]}>Profile</div>;
+    return (
+        <div className={styles["profile-container"]}>
+            <h3>Thecon</h3>
+            <p>Temaa</p>
+            <p>Temaa</p>
+            <p>Temaa</p>
+            <p>Temaa</p>
+            <p>Temaa</p>
+            <p>Temaa</p>
+            <p>Temaa</p>
+        </div>
+    );
 };
 
 export default Profile;
