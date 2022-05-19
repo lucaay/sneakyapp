5a52f11214b0e79facc3d136d621e7abebe38531
Luca Sergiu <you@example.com>
2022-05-19 20:14:34 +0300

    chaanges to layout

---
+++ src/components/Dashboard/Content/AddStage/AddStage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./AddStage.module.css";
+
+const AddStage = () => {
+    return <div>AddStage</div>;
+};
+
+export default AddStage;
--- src/components/Dashboard/Content/Content.js
+++ src/components/Dashboard/Content/Content.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AddStage from "./AddStage/AddStage";
 import styles from "./Content.module.css";
 
 const Content = () => {
-    return <div>Content</div>;
+    return (
+        <div className={styles["content-container"]}>
+            <Routes>
+                <Route path="/test-page" element={<AddStage />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default Content;
