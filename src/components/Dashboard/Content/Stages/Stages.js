159a558f2acd2dbcf59ead467c52159c29eac9b6
Luca Sergiu <you@example.com>
2022-05-20 01:01:25 +0300

    chaanges to lists

--- src/components/Dashboard/Content/Stages/Stages.js
+++ src/components/Dashboard/Content/Stages/Stages.js
@@ -1,9 +1,48 @@
 import React, { useEffect, useState } from "react";
 import StageCard from "./StageCard/StageCard";
 import styles from "./Stages.module.css";
+import { getUserEmail } from "../../../Firebase/firebase";
 
 const Stages = () => {
     const [stages, setStages] = useState(null);
+
+    const [numeFirmaCurenta, setNumeFirmaCurenta] = useState(null);
+    const [rolCont, setRolCont] = useState(null);
+
+    let currentUserEmail;
+    useEffect(() => {
+        setTimeout(() => {
+            currentUserEmail = getUserEmail();
+            let url =
+                "https://sneakyapp-e098d-default-rtdb.firebaseio.com/users.json";
+            const fetchUsers = async () => {
+                const response = await fetch(url);
+                const responseData = await response.json();
+
+                let loadedFirme = "";
+
+                for (const key in responseData) {
+                    if (
+                        responseData[key].rol === "firma" &&
+                        responseData[key].email === currentUserEmail
+                    ) {
+                        loadedFirme = responseData[key].nume;
+                    }
+                    if (
+                        responseData[key].rol === "student" &&
+                        responseData[key].email === currentUserEmail
+                    ) {
+                        setRolCont(responseData[key].rol);
+                    }
+                }
+
+                setNumeFirmaCurenta(loadedFirme);
+            };
+
+            fetchUsers();
+        }, 1000);
+    }, []);
+
     useEffect(() => {
         const fetchStages = async () => {
             const response = await fetch(
@@ -24,12 +63,18 @@ const Stages = () => {
                     orar: responseData[key].orar,
                 });
             }
-
-            setStages(loadedStages);
+            if (rolCont !== "student") {
+                const newLoadedStages = loadedStages.filter(
+                    (item) => item.firma === numeFirmaCurenta
+                );
+                setStages(newLoadedStages);
+            } else {
+                setStages(loadedStages);
+            }
         };
 
         fetchStages();
-    }, []);
+    }, [numeFirmaCurenta, rolCont]);
     return (
         <div className={styles["stages-container"]}>
             {stages?.map((stagiu) => (
