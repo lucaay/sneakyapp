5989eaea70993ce55345a46e47123a0d3df86e1b
Luca Sergiu <you@example.com>
2022-05-20 01:19:29 +0300

    profile page variables

--- src/components/Dashboard/Content/Profile/Profile.js
+++ src/components/Dashboard/Content/Profile/Profile.js
@@ -1,23 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Profile.module.css";
 import logo from "../../../../assets/logo.png";
+import { getUserEmail } from "../../../Firebase/firebase";
 
-const Profile = () => {
+const Profile = (props) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [userData, setUserData] = useState(null);
+
+    let currentUserEmail;
+    useEffect(() => {
+        setIsLoading(true);
+        setTimeout(() => {
+            currentUserEmail = getUserEmail();
+            let url =
+                "https://sneakyapp-e098d-default-rtdb.firebaseio.com/users.json";
+            const fetchUsers = async () => {
+                const response = await fetch(url);
+                const responseData = await response.json();
+
+                const loadedUsers = [];
+
+                for (const key in responseData) {
+                    loadedUsers.push({
+                        id: key,
+                        email: responseData[key].email,
+                        nume: responseData[key].nume,
+                        prenume: responseData[key]?.prenume,
+                        rol: responseData[key].rol,
+                        an: responseData[key]?.an,
+                        dataNastere: responseData[key]?.dataNastere,
+                        facultate: responseData[key]?.facultate,
+                        judet: responseData[key]?.judet,
+                        specializare: responseData[key]?.specializare,
+                        firmaTutore: responseData[key]?.firmaTutore,
+                        cui: responseData[key]?.cui,
+                        domeniuActivitate: responseData[key]?.domeniuActivitate,
+                    });
+                }
+
+                const user = loadedUsers.filter(
+                    (item) => item.email === currentUserEmail
+                );
+
+                setUserData(user[0]);
+                setIsLoading(false);
+            };
+
+            fetchUsers();
+        }, 1000);
+    }, []);
     return (
-            <div className={styles["profile-container"]}>
-                <h1 className={styles.titlu}>Profil</h1>
-                    <img className={styles.avatar} src={logo} />
-                    <div className={styles['text']}>
-                        
-                        <p>Nume: Gicu</p>
-                        <p>Prenume:</p>
-                        <p>Temaa</p>
-                        <p>Temaa</p>
-                        <p>Temaa</p>
-                        <p>Temaa</p>
-                        <p>Temaa</p>
-                    </div>
-            </div>
+        <div className={styles["profile-container"]}>
+            <h1 className={styles.titlu}>Profil</h1>
+            <img className={styles.avatar} src={logo} />
+            {isLoading && "Se încarcă..."}
+            {!isLoading && (
+                <div className={styles["text"]}>
+                    <p>Nume: {userData.nume}</p>
+                    <p>
+                        {userData?.prenume && "Prenume: " + userData?.prenume}
+                    </p>
+                    <p>E-mail: {userData.email}</p>
+                    <p>Tip cont: {userData.rol}</p>
+                    <p>
+                        {userData?.dataNastere &&
+                            "Data Nașterii: " + userData?.dataNastere}
+                    </p>
+                    <p>
+                        {userData?.facultate &&
+                            "Facultate: " + userData?.facultate}
+                    </p>
+                    <p>{userData?.judet && "Județ: " + userData?.judet}</p>
+                    <p>
+                        {userData?.specializare &&
+                            "Specializare: " + userData?.specializare}
+                    </p>
+                    <p>{userData?.an && "An studiu: " + userData?.an}</p>
+                    <p>{userData?.CUI && "CUI: " + userData?.CUI}</p>
+                    <p>
+                        {userData?.domeniuActivitate &&
+                            "Domeniu Activitate: " +
+                                userData?.domeniuActivitate}
+                    </p>
+                    <p>
+                        {userData?.firmaTutore &&
+                            "Firmă Tutore: " + userData?.firmaTutore}
+                    </p>
+                </div>
+            )}
+        </div>
     );
 };
 
